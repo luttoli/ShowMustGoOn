dc4cc7cdf91363623cc4e1d2dd1833974d3dad64
luttoli <hhn0212@naver.com>
2024-10-23 01:50:44 +0900

    [FEAT] segment animate, bottomLine

--- ShowMustGoOn/Scene/ListView/ListViewController.swift
+++ ShowMustGoOn/Scene/ListView/ListViewController.swift
@@ -16,46 +16,26 @@ class ListViewController: UIViewController {
     
     // MARK: - Components
     var listSegment: UISegmentedControl = {
-        let listSegment = UISegmentedControl(items: ["테이블뷰", "콜랙션뷰", "메뉴뷰", "선택뷰"])
+        let listSegment = UISegmentedControl(items: ["테이블뷰", "콜랙션뷰", "카테고리", "선택하기"])
         listSegment.selectedSegmentIndex = 0
         
-        let nomal: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.text.button.disabled,
-            .font: UIFont.toPretendard(size: Constants.size.size10, weight: .SemiBold)
-        ]
+        // 선택, 미선택 타이틀설정
+        listSegment.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.toPretendard(size: Constants.size.size15, weight: .Regular), .foregroundColor: UIColor.text.subDarkGray], for: .normal)
+        listSegment.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.toPretendard(size: Constants.size.size15, weight: .SemiBold), .foregroundColor: UIColor.text.black], for: .selected)
         
-        let selected: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.text.button.black,
-            .font: UIFont.toPretendard(size: Constants.size.size15, weight: .SemiBold)
-        ]
+        // 배경색 설정
+        listSegment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default) // 배경색 변경
+        listSegment.setBackgroundImage(UIImage(), for: .highlighted, barMetrics: .default) // 선택된 색 변경 -> 흰색
         
-        listSegment.setTitleTextAttributes(nomal, for: .normal)
-        listSegment.setTitleTextAttributes(selected, for: .selected)
-        
-        let clearImage = UIImage()
-        listSegment.setBackgroundImage(clearImage, for: .normal, barMetrics: .default) // 배경색 변경
-        listSegment.setBackgroundImage(clearImage, for: .selected, barMetrics: .default) // 선택된 색 변경 -> 흰색
-        listSegment.setDividerImage(clearImage, forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default) // 구분선 흰색
-        
+        // 구분선 설정
+        listSegment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default) // 구분선 흰색
         return listSegment
     }()
     
-    let firstView: UIView = {
-        let firstView = UIView()
-        firstView.backgroundColor = .red
-        return firstView
-    }()
-    
-    let secondView: UIView = {
-        let secondView = UIView()
-        secondView.backgroundColor = .blue
-        return secondView
-    }()
-    
-    let thirdView: UIView = {
-        let thirdView = UIView()
-        thirdView.backgroundColor = .green
-        return thirdView
+    var bottomLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .button.lavender
+        return view
     }()
     
     init() {
@@ -79,6 +59,11 @@ extension ListViewController {
         setUp()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateBottomLinePosition()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         listSegment.selectedSegmentIndex = 0
     }
@@ -98,63 +83,45 @@ extension ListViewController {
 private extension ListViewController {
     func setUp() {
         view.addSubview(listSegment)
+        view.addSubview(bottomLineView)
         
         listSegment.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.vertical)
-            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.horizontal)
-            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.horizontal)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalTo(view.safeAreaLayoutGuide)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(Constants.size.size50)
         }
-
-        view.addSubview(firstView)
-        firstView.snp.makeConstraints {
-            $0.top.equalTo(listSegment.snp.bottom).offset(Constants.margin.vertical)
-            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.horizontal)
-            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.horizontal)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.vertical)
-        }
-        
         listSegment.addAction(UIAction(handler: { [weak self] _ in
-            self?.changeView()
+            self?.animateSelectedSegment(segment: self!.listSegment)
+            self?.updateBottomLinePosition()
         }), for: .valueChanged)
     }
 }
 
 // MARK: - Method
 private extension ListViewController {
-    func changeView() {
-        if listSegment.selectedSegmentIndex == 0 {
-            // 첫 번째 뷰를 보여줌
-            secondView.removeFromSuperview() // 다른 뷰는 제거
-            thirdView.removeFromSuperview()
-            view.addSubview(firstView)
-            firstView.snp.makeConstraints {
-                $0.top.equalTo(listSegment.snp.bottom).offset(Constants.margin.vertical)
-                $0.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.horizontal)
-                $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.horizontal)
-                $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.vertical)
+    func animateSelectedSegment(segment: UISegmentedControl) {
+        UIView.animate(withDuration: 0.3) {
+            segment.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3) {
+                segment.transform = CGAffineTransform.identity
             }
-        } else if listSegment.selectedSegmentIndex == 1 {
-            // 두 번째 뷰를 보여줌
-            firstView.removeFromSuperview() // 다른 뷰는 제거
-            thirdView.removeFromSuperview()
-            view.addSubview(secondView)
-            secondView.snp.makeConstraints {
-                $0.top.equalTo(listSegment.snp.bottom).offset(Constants.margin.vertical)
-                $0.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.horizontal)
-                $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.horizontal)
-                $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.vertical)
-            }
-        } else {
-            firstView.removeFromSuperview()
-            thirdView.removeFromSuperview()
-            view.addSubview(thirdView)
-            thirdView.snp.makeConstraints {
-                $0.top.equalTo(listSegment.snp.bottom).offset(Constants.margin.vertical)
-                $0.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.margin.horizontal)
-                $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.horizontal)
-                $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-Constants.margin.vertical)
-            }
+        }
+    }
+    
+    private func updateBottomLinePosition() {
+        let selectedSegmentIndex = listSegment.selectedSegmentIndex
+        let segmentWidth = listSegment.bounds.width / CGFloat(listSegment.numberOfSegments)
+        let lineHeight: CGFloat = 3 // 바텀 라인의 높이
+
+        UIView.animate(withDuration: 0.3) {
+            self.bottomLineView.frame = CGRect(
+                x: CGFloat(selectedSegmentIndex) * segmentWidth,
+                y: self.listSegment.frame.maxY - lineHeight,
+                width: segmentWidth,
+                height: lineHeight
+            )
         }
     }
 }
