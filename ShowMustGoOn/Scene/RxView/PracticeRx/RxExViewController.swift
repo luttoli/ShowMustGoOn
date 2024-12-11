27e5eccdf4dc445e705fc95e9bc69c374ba5ca3f
luttoli <hhn0212@naver.com>
2024-12-11 22:25:19 +0900

    [Practice] UI와 RxSwift 연동 - 과제 1 버튼 클릭 시 텍스트필드에 입력값 UILabel 출력 입력값 없이 버튼 누르는 상황 처리 개선 - 입력값 삭제 시 UILabel 초기값으로 변경 처리

--- ShowMustGoOn/Scene/RxView/PracticeRx/RxExViewController.swift
+++ ShowMustGoOn/Scene/RxView/PracticeRx/RxExViewController.swift
@@ -16,7 +16,27 @@ class RxExViewController: UIViewController {
     let disposeBag = DisposeBag()
     
     // MARK: - Components
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "여기에 입력하세요."
+        return textField
+    }()
     
+    let button = CustomButton(type: .textButton(title: "클릭하세요!", color: .lavender, size: .small))
+    
+    let labelText = BehaviorRelay<String>(value: "버튼 클릭하면 여기에 표시됩니다.")
+    
+    let label = CustomLabel(title: "", size: Constants.size.size20, weight: .Regular)
+    
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [textField, button, label])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+        return stackView
+    }()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -36,25 +56,40 @@ extension RxExViewController {
         view.backgroundColor = .white
         
         setUp()
+        didTapButton()
     }
 }
 
 // MARK: - SetUp
 private extension RxExViewController {
     func setUp() {
+        view.addSubview(stackView)
         
+        stackView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        textField.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+        }
     }
 }
 
 // MARK: - Method
 private extension RxExViewController {
-    //    func <#name#>() {
-    //
-    //    }
+    func didTapButton() {
+        // 버튼 클릭 시 텍스트 필드 값에 따라 레이블 업데이트
+        button.rx.tap
+            .withLatestFrom(textField.rx.text.orEmpty)
+            .map { $0.isEmpty ? "값을 입력하세요." : $0 }
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+        
+        textField.rx.text.orEmpty
+            .filter { $0.isEmpty } // 텍스트가 비어있는 경우만 처리
+            .map { _ in self.labelText.value } // 초기값 설정
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
-
-// MARK: - Delegate
-extension RxExViewController {
-    
-}
-
