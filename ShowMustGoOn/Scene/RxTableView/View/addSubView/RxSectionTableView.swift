48f4b352abf0b3436a205b51d849de41e0d18f36
luttoli <hhn0212@naver.com>
2024-12-19 11:03:03 +0900

    [Practice] RxSection 구구단 TableView 구현 2단 ~ 9단 클릭 시 계산 결과값 ? -> 클릭 시 계산된 값으로 변경 예정 단마다 section 나눌 예정

--- ShowMustGoOn/Scene/RxTableView/View/addSubView/RxSectionTableView.swift
+++ ShowMustGoOn/Scene/RxTableView/View/addSubView/RxSectionTableView.swift
@@ -13,14 +13,20 @@ import SnapKit
 
 class RxSectionTableView: UIView {
     // MARK: - Properties
-    
+    let disposeBag = DisposeBag()
+    let viewModel = RxSectionViewModel()
     
     // MARK: - Components
-    
+    let multiplicationTableView: UITableView = {
+        let multiplicationTableView = UITableView()
+        multiplicationTableView.register(MultiplyTableViewCell.self, forCellReuseIdentifier: MultiplyTableViewCell.identifier)
+        return multiplicationTableView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUp()
+        bindTableView()
     }
     
     required init?(coder: NSCoder) {
@@ -31,16 +37,33 @@ class RxSectionTableView: UIView {
 // MARK: - SetUp
 private extension RxSectionTableView {
     func setUp() {
-        backgroundColor = .red
+        addSubview(multiplicationTableView)
+        
+        multiplicationTableView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.equalTo(safeAreaLayoutGuide)
+            $0.trailing.equalTo(safeAreaLayoutGuide)
+            $0.bottom.equalTo(safeAreaLayoutGuide)
+        }
     }
 }
 
 // MARK: - Method
 extension RxSectionTableView {
-
-}
-
-// MARK: - delegate
-extension RxSectionTableView {
-    
+    func bindTableView() {
+        viewModel.multiplyData
+            .bind(to: multiplicationTableView.rx.items(cellIdentifier: MultiplyTableViewCell.identifier)) { index, data, cell in
+                guard let cell = cell as? MultiplyTableViewCell else { return }
+                cell.frontNumberLabel.text = "\(data.frontNumber)"
+                cell.backNumberLabel.text = "\(data.backNumber)"
+                cell.resultNumberLabel.text = data.resultNumber
+            }
+            .disposed(by: disposeBag)
+        
+        multiplicationTableView.rx.modelSelected(SectionModel.self)
+            .subscribe(onNext: { model in
+                print("\(model.frontNumber * model.backNumber)")
+            })
+            .disposed(by: disposeBag)
+    }
 }
--- ShowMustGoOn/Scene/RxTableView/ViewModel/RxSectionViewModel.swift
+++ ShowMustGoOn/Scene/RxTableView/ViewModel/RxSectionViewModel.swift
@@ -10,5 +10,21 @@ import UIKit
 import RxSwift
 
 class RxSectionViewModel {
-
+    let disposeBag = DisposeBag()
+    
+    let frontNumbers = Array(2...9)
+    let backNumbers = Array(1...9)
+    
+    lazy var multiplyData: Observable<[SectionModel]> = {
+        let data = frontNumbers.flatMap { front in
+            backNumbers.map { back in
+                SectionModel(
+                    frontNumber: front,
+                    backNumber: back,
+                    resultNumber: "?"
+                )
+            }
+        }
+        return Observable.just(data)
+    }()
 }
