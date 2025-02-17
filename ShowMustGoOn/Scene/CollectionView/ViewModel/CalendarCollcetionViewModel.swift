085032886572eddd3bededff5ba2c7f7ff6e91a6
luttoli <hhn0212@naver.com>
2025-02-17 15:36:44 +0900

    [Practice] CalendarCollectionView 한국 일시 설정

--- ShowMustGoOn/Scene/CollectionView/ViewModel/CalendarCollcetionViewModel.swift
+++ ShowMustGoOn/Scene/CollectionView/ViewModel/CalendarCollcetionViewModel.swift
@@ -9,17 +9,24 @@ import UIKit
 
 class CalendarCollcetionViewModel {
     // 캘린더 인스턴스
-    var calendar = Calendar.current
+    var calendar: Calendar = {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = Locale(identifier: "ko_KR") // 한국 로케일
+        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")! // 한국 시간대
+        return calendar
+    }()
     
     // DateFormatter 설정
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy년 MM월"
+        formatter.locale = Locale(identifier: "ko_KR") // 한국어 표시
+        formatter.timeZone = TimeZone(identifier: "Asia/Seoul")! // 한국 시간대
         return formatter
     }()
     
     // 현재 날짜
-    private(set) var calendarDate = Date()
+    var calendarDate = Date()
     
     // 뷰에서 구독할 수 있도록 프로퍼티 추가
     var yearLabelText: String {
